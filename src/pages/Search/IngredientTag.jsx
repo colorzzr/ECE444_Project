c3b09d46c3c661bc5db119e2870b48cdc1119c1c
Justin Leung <justinpc.leung@mail.utoronto.ca>
2020-11-03 20:50:41 -0800

    Added search by ingredient tab

---
+++ src/pages/Search/IngredientTag.jsx
@@ -0,0 +1,150 @@
+import React, { Component } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+import { Tag, Input, Tooltip, Button } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+
+const onSearch = (value) => console.log(value);
+
+class IngredientTag extends Component {
+  state = {
+    tags: [],
+    inputVisible: true,
+    inputValue: '',
+    editInputIndex: '',
+    editInputValue: '',
+  };
+
+  handleClose = (removedTag) => {
+    const tags = this.state.tags.filter((tag) => tag !== removedTag);
+    console.log(tags);
+    this.setState({ tags });
+  };
+
+  showInput = () => {
+    this.setState({ inputVisible: true }, () => this.input.focus());
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleInputConfirm = () => {
+    const { inputValue } = this.state;
+    let { tags } = this.state;
+    if (inputValue && tags.indexOf(inputValue) === -1) {
+      tags = [...tags, inputValue];
+    }
+    console.log(tags);
+    this.setState({
+      tags,
+      inputVisible: true,
+      inputValue: '',
+    });
+  };
+
+  handleEditInputChange = (e) => {
+    this.setState({ editInputValue: e.target.value });
+  };
+
+  handleEditInputConfirm = () => {
+    this.setState(({ tags, editInputIndex, editInputValue }) => {
+      const newTags = [...tags];
+      newTags[editInputIndex] = editInputValue;
+
+      return {
+        tags: newTags,
+        editInputIndex: '',
+        editInputValue: '',
+      };
+    });
+  };
+
+  saveInputRef = (input) => {
+    this.input = input;
+  };
+
+  saveEditInputRef = (input) => {
+    this.editInput = input;
+  };
+
+  render() {
+    const { tags, inputVisible, inputValue, editInputIndex, editInputValue } = this.state;
+    return (
+      <>
+        <Button type="primary" icon={<SearchOutlined />}>
+          Search
+        </Button>
+        <br /> <br />
+        <Input
+          placeholder="Enter an Ingredient"
+          allowClear
+          enterButton="Add"
+          size="large"
+          onSearch={onSearch}
+          style={{ width: '25%' }}
+          ref={this.saveInputRef}
+          type="text"
+          className="tag-input"
+          value={inputValue}
+          onChange={this.handleInputChange}
+          onBlur={this.handleInputConfirm}
+          onPressEnter={this.handleInputConfirm}
+        ></Input>
+        <br />
+        <br />
+        {tags.map((tag, index) => {
+          if (editInputIndex === index) {
+            return (
+              <Input
+                ref={this.saveEditInputRef}
+                key={tag}
+                size="large"
+                className="tag-input"
+                value={editInputValue}
+                onChange={this.handleEditInputChange}
+                onBlur={this.handleEditInputConfirm}
+                onPressEnter={this.handleEditInputConfirm}
+              />
+            );
+          }
+
+          const isLongTag = tag.length > 20;
+
+          const tagElem = (
+            <Tag
+              className="edit-tag"
+              key={tag}
+              closable={index >= 0}
+              onClose={() => this.handleClose(tag)}
+              style={{ lineHeight: '32px', fontSize: '20px' }}
+            >
+              <span
+                onDoubleClick={(e) => {
+                  if (index !== 0) {
+                    this.setState({ editInputIndex: index, editInputValue: tag }, () => {
+                      this.editInput.focus();
+                    });
+                    e.preventDefault();
+                  }
+                }}
+              >
+                {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+              </span>
+            </Tag>
+          );
+          return isLongTag ? (
+            <Tooltip title={tag} key={tag}>
+              {tagElem}
+            </Tooltip>
+          ) : (
+            tagElem
+          );
+        })}
+        <br />
+        <br />
+      </>
+    );
+  }
+}
+
+export default IngredientTag;
--- src/pages/Search/SearchRecipe.jsx
+++ src/pages/Search/SearchRecipe.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Tabs } from 'antd';
+import { Tabs, PageHeader } from 'antd';
 import axios from 'axios';
 const { TabPane } = Tabs;
 
 import defaultSettings from '../../../config/defaultSettings';
+import IngredientTag from './IngredientTag';
 const { api_endpoint } = defaultSettings;
 
 function changeSearchOption(key) {
@@ -13,14 +14,13 @@ function changeSearchOption(key) {
 const SearchOptions = () => (
   <Tabs defaultActiveKey="1" onChange={changeSearchOption}>
     <TabPane tab="Search Recipe by Ingredients" key="1">
-      Content of Tab Pane 1
+      <PageHeader title="Find a Recipe"></PageHeader>
+      <IngredientTag />
     </TabPane>
     <TabPane tab="Search Recipe by Name" key="2">
       Content of Tab Pane 2
     </TabPane>
-    <TabPane tab="Search Recipe by Pantry Items" key="3">
-      Content of Tab Pane 3
-    </TabPane>
+    <TabPane tab="Search Recipe by Pantry Items" key="3"></TabPane>
   </Tabs>
 );
 
